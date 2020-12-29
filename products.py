d2056ff69175a47936d2e35d4d8fcf9b77378ba7
Stanleytong <2010stanleytong@gmail.com>
2020-12-29 16:50:15 +0800

    add products

---
+++ products.py
@@ -0,0 +1,11 @@
+products = []
+
+while True:
+	name = input('請輸入產品名稱： ')
+	if name == 'q':
+		break
+	price = input('請輸入產品價錢： ')
+	products.append([name, price])
+print(products)
+
+print (products[0][1])
